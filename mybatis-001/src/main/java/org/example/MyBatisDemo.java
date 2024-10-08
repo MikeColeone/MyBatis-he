71360ba8e621b66e6dcd5129ee005d3cf591952e
MikeColeone <coleonemike@gmail.com>
2024-10-08 23:00:30 +0800

    UserMapper

--- mybatis-001/src/main/java/org/example/MyBatisDemo.java
+++ mybatis-001/src/main/java/org/example/MyBatisDemo.java
@@ -1,18 +1,28 @@
-package org.mybatis.example;
+package org.example;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.pojo.User;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
-public class Main {
+public class MyBatisDemo {
     public static void main(String[] args) throws IOException {
         //使用配置文件构建sqlSessionFactory
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        //获取sqlSession
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            List<User> users = session.selectList("UserMapper.selectAllUsers", 101);
+            System.out.println(users);
+        }
 
         /*
         //不使用配置文件构建sqlSession
@@ -25,9 +35,7 @@ public class Main {
          */
 
         //从sqlSessionFactory获取SqlSession
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//            Blog blog = (Blog) session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
-//        }
+
 
         //通过一下该方式获取sqlSession避免强转
         /*
---
+++ mybatis-001/src/main/java/org/example/pojo/User.java
@@ -0,0 +1,24 @@
+package org.example.pojo;
+
+public class User {
+    private String id;
+    private String name;
+    private String createTime;
+    private String updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+}
